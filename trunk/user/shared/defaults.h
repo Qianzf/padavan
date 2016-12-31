9189855031fcac8da5875e5062ef88ad74cee19e
liveup <kingf_c@foxmail.com>
2016-12-31 10:14:12 +0800

    change 5G country_code as USA,more channels

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -34,7 +34,7 @@
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
-#define DEF_WLAN_5G_CC		"GB"
+#define DEF_WLAN_5G_CC		"US"
 #define DEF_WLAN_2G_SSID	"ASUS"
 #define DEF_WLAN_5G_SSID	"ASUS_5G"
 #define DEF_WLAN_2G_GSSID	"ASUS_GUEST"
